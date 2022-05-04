5c507ff8fe0443c6b80e1cef6d299a43a806b595
nancy <nancypurwar5@gmail.com>
2022-05-04 12:06:32 +0530

    add student.js class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        hello reactjs
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    return <>
+    
+    <h1>hello</h1>
+    <Header title="header 1"/>
+    <About/>
+    <Footer/>
+    </>
 }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './About.css';
+const Header= (props)=>{
+
+    return(<>
+    <h1 id="about">hello {props.title}</h1></>)
+
+  
+}
+export default Header;
